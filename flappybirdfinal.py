b0c43ad4fd8dcb26c4199aa9a7f4c5007eecc017
Aavas Sedhai <asedhai@gmail.com>
2021-08-28 13:15:16 +0545

    bird class and properties

--- flappybirdfinal.py
+++ flappybirdfinal.py
@@ -11,3 +11,60 @@ FPS = 60
 ANIMATION_SPEED = 0.18  # pixels per millisecond
 WIN_WIDTH = 284 * 2     # BG image size: 284x512 px; tiled twice
 WIN_HEIGHT = 512
+
+class Bird(pygame.sprite.Sprite):
+    """Represents the bird controlled by the player.
+    """
+
+    WIDTH = HEIGHT = 50
+    SINK_SPEED = 0.18
+    CLIMB_SPEED = 0.3
+    CLIMB_DURATION = 333.3
+
+    def __init__(self, x, y, msec_to_climb, images):
+        """Initialise a new Bird instance.
+        """
+        super(Bird, self).__init__()
+        self.x, self.y = x, y
+        self.msec_to_climb = msec_to_climb
+        self._img_wingup, self._img_wingdown = images
+        self._mask_wingup = pygame.mask.from_surface(self._img_wingup)
+        self._mask_wingdown = pygame.mask.from_surface(self._img_wingdown)
+
+    def update(self, delta_frames=1):
+        """Update the bird's position
+        delta_frames: The number of frames elapsed since this method was
+            last called.
+        """
+        if self.msec_to_climb > 0:
+            frac_climb_done = 1 - self.msec_to_climb/Bird.CLIMB_DURATION
+            self.y -= (Bird.CLIMB_SPEED * frames_to_msec(delta_frames) *
+                       (1 - math.cos(frac_climb_done * math.pi)))
+            self.msec_to_climb -= frames_to_msec(delta_frames)
+        else:
+            self.y += Bird.SINK_SPEED * frames_to_msec(delta_frames)
+
+    @property
+    def image(self):
+        """Get a Surface containing this bird's image
+        """
+        if pygame.time.get_ticks() % 500 >= 250:
+            return self._img_wingup
+        else:
+            return self._img_wingdown
+
+    @property
+    def mask(self):
+        """Get a bitmask for use in collision detection.
+
+        bitmask excludes all pixels in self.image with a
+        transparency greater than 127."""
+        if pygame.time.get_ticks() % 500 >= 250:
+            return self._mask_wingup
+        else:
+            return self._mask_wingdown
+
+    @property
+    def rect(self):
+        """Get the bird's position, width, and height"""
+        return Rect(self.x, self.y, Bird.WIDTH, Bird.HEIGHT)
